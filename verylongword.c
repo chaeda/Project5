6459b334f7b429ad5eb813cc9199215aa3d7faee
suvin <42831266+chaeda@users.noreply.github.com>
2018-10-09 16:14:05 +0900

    Update verylongword.c

--- verylongword.c
+++ verylongword.c
@@ -4,7 +4,7 @@ int main()
 {
 	char string[100], word[20], max[20], min[20], c;
 	int i = 0, j = 0, sen = 0;
-	printf("´Ü¾î ÀÔ·Â: ");
+	printf("ë‹¨ì–´ ìž…ë ¥: ");
 	gets(string);
 	for (i = 0; i < strlen(string); i++)
 	{
@@ -27,30 +27,7 @@ int main()
 			j = 0;
 		}
 	}
-	printf("°¡Àå ±ä ´Ü¾î´Â: '%s' .\n", max);
+	printf("ê°€ìž¥ ê¸´ ë‹¨ì–´ëŠ”: '%s' .\n", max);
 	system("pause");
-	return 0;
-}
-*/
-#include <stdio.h>
-
-int main() {
-	char string[100] = "´Ü¾î ÀÔ·Â";
-	int i, start = 0, longest = 0, longest_pos = 0;
-
-	for (i = 0; string[i] != '\0'; i++) {
-		if (string[i] == ' ') {
-			start = i + 1;
-		}
-		else {
-			if (i - start > longest) {
-				longest = i - start;
-				longest_pos = start;
-			}
-		}
-	}
-	printf("longest word: %d letters, '%.*s'\n",
-		longest, longest, string + longest_pos);
-
 	return 0;
 }
